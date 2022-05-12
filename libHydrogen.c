cf4b01c914a446d9cadcb40b9ad0a60f3425f9b1
Conordoherty165 <75123069+Conordoherty165@users.noreply.github.com>
2022-05-12 16:46:03 +0100

    Update libHydrogen.c
    fixing for displaying the encrypted message correctly

--- libHydrogen.c
+++ libHydrogen.c
@@ -1,27 +1,43 @@
 #include "hydrogen.h"
 #include <stdint.h>
 #include <stdio.h>
-#define CONTEXT "Examples"
-#define MESSAGE "Hello World!"
-#define MESSAGE_LEN 12
-#define CIPHERTEXT_LEN (hydro_secretbox_HEADERBYTES + MESSAGE_LEN)
+#include <string.h>
 
-uint8_t key[hydro_secretbox_KEYBYTES];
-uint8_t ciphertext[CIPHERTEXT_LEN];
-char decrypted[MESSAGE_LEN];
+#define CONTEXT "Examples"
 
 int main(int argc, char **argv) {
-	if (hydro_init() != 0) {
-	        abort();
-	    }
-	hydro_secretbox_keygen(key);
-	hydro_secretbox_encrypt(ciphertext, MESSAGE, MESSAGE_LEN, 0, CONTEXT, key);
+    // Check that hydrogen can do encryption
+	if (hydro_init() != 0 || argc != 2) {
+        printf("Error initializing Hydrogen or wrong number of arguments passed (expected %d, got %d)\n", 1, argc - 1);
+        return EXIT_FAILURE;
+	}
 
-	if (hydro_secretbox_decrypt(decrypted, ciphertext, CIPHERTEXT_LEN, 0, CONTEXT, key) != 0) {
-	    /* message forged! */
+    // Get the command line argument
+    char *message = argv[1];
+    int len = strlen(message);
+    int cipher_len = hydro_secretbox_HEADERBYTES + len;
+
+    // Initialize array memory space
+    uint8_t *key = malloc(hydro_secretbox_KEYBYTES * sizeof(char));
+    uint8_t *ciphertext = malloc(cipher_len * sizeof(char));
+    uint8_t *decrypted = malloc(len * sizeof(char));
+
+    // Generate the key
+	hydro_secretbox_keygen(key);
+
+    // Try to encrypt the message with our key, exit on failure
+    if (hydro_secretbox_encrypt(ciphertext, message, len, 0, CONTEXT, key) != 0) {
+        printf("Error encrypting");
+        return EXIT_FAILURE;
+    }
+
+    // Try to decrypt the cipher text with our key, exit on failure
+    // Print everything on success to show message was successfully encrypted/decrypted
+	if (hydro_secretbox_decrypt(decrypted, ciphertext, cipher_len, 0, CONTEXT, key) != 0) {
+        printf("Error decrypting, message is not authentic\n");
+        return EXIT_FAILURE;
+	} else {
+		printf("Original message: %s\nEncrypted: %s\nDecrypted: %s\n", message, ciphertext, decrypted);
 	}
-	else{
-		printf("message: %s \n  ciphertext: %s \n  decrypted: %s \n",MESSAGE,ciphertext,decrypted);
-	}
-	return 0;
+	return EXIT_SUCCESS;
 }
